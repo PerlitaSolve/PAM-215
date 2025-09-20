586e8c4561f5af32ba3f99c93a402e4b7960787c
PerlitaSolve <124049201@upq.edu.mx>
2025-09-19 20:31:35 -0600

    Cambio de variables "var" por "let" y "const" según corresponda

---
+++ Practica 1/Ejerr1.js
@@ -0,0 +1,9 @@
+
+let nombre="Armando";
+const edad=25;
+
+nombre= "Ana Maria";
+
+const saludo ="hola "+nombre+" tienes "+edad+" años"; 
+// muestra en consola en contenido de saludo
+console.log(saludo);
