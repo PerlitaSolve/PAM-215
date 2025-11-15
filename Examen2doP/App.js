e26fe18546b0e18b4cd6cca085d341e416d19502
PerlitaSolve <124049201@upq.edu.mx>
2025-11-14 19:19:55 -0600

    BottomSheet completada

--- Examen2doP/App.js
+++ Examen2doP/App.js
@@ -1,6 +1,5 @@
-import {StyleSheet, Text, View, Animated,ScrollView, ImageBackground, Easing, Button, Switch  } from 'react-native';
-import React, { useState, useEffect } from 'react';
-
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
 export default function App() {
   const scrollRef=useRef();
@@ -29,117 +28,13 @@ export default function App() {
   // }
 
   return (
-    <ImageBackground
-    source={require('./assets/Exa.jpg')}
-    style={styles.background}
-    resizeMode="cover"
-    >
     <View>
-
-        <View>
-          <Text>MIS DEBERES</Text>
-        </View>
-
-        <View>
-          <Text>Fecha=07/ Noviembre/2025</Text>
-        </View>
-
+      <Text>App</Text>
     </View>
-     
-
-      <ScrollView
-      ref={scrollRef}
-      style={styles.container}
-      contentContainerStyle={styles.content}
-      showsVerticalScrollIndicator={true}
-      >
-
-
-        <View style={styles.categoria1}>
-          <Text>CATEGORIAS</Text>
-          <Text style={styles.sub}>HOGAR</Text>
-          <View>
-    
-          <Text>1. Recoger cuarto</Text>
-          <Text>Ordenar recamara principal</Text>
-          <Text>Ordenar recamara principal</Text>
-          </View>
-          <Switch>
-            <Text>{cambiarCompletada ? 'Completada' : 'Pendiente'}</Text>
-          </Switch>
-          
-          <View>
-
-          <Text>2. Lavar ropa</Text>
-          <Text>Ordenar recamara principal</Text>
-          <Text>Ordenar recamara principal</Text>
-          </View>
-          
-          <Switch>
-            <Text>{cambiarCompletada ? 'Completada' : 'Pendiente'}</Text>
-          </Switch>
-          </View>
-        
-
-
-         <View style={styles.categoria2}>
-          <Text style={styles.sub}>ESCUELA</Text>
-        </View>
-
-         <View style={styles.categoria3}>
-          <Text style={styles.sub}>PERSONAL</Text>
-        </View>
-
-      </ScrollView>
-
-      
-
-    </ImageBackground>
-   
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  background:{
-    alignContent:'center',
-    flex:1,
-    justifyContent:'center',
-    width:'100%',
-    height:'100%',
-  },
-  // splashContainer:{
-  //   flex:1,
-  //   justifyContent:'center',
-  //   alignItems:'center',
-  //   backgroundColor:'#f8c6ecff',
-  //   padding:50,
-  // },
-  titulo:{
-    fontFamily: 'Times New Roman',
-    fontSize: 40,
-    color: '#050b8eff',
-  },
-  sub:{
-    fontSize: 16,
-    fontFamily:'Courier',
-    color: '#7b7b7bff',
-    fontWeight: '900', 
-  },
-  categoria:{
-    backgroundColor:'#ca9af7bb',
-    width: '100%',
-    height: 100,
-    borderRadius:10,
-    marginVertical:10,
-  },
-  content:{
-    padding: 20,
-    paddingBottom: 40,
-  },
-});
+
+
+
+const styles = StyleSheet.create({})
